4e2e630a544db6c13c0c4e7b0a81ece74d1fe30b
algapry <alihameed7121996ph@gmail.com>
2025-06-13 13:41:43 +0300

    update api

--- app/api/views/[id]/route.ts
+++ app/api/views/[id]/route.ts
@@ -3,8 +3,8 @@ import { writeClient } from "@/sanity/lib/write-client";
 import { client } from "@/sanity/lib/client";
 import { STARTUP_VIEWS_QUERY } from "@/sanity/lib/queries";
 
-export async function POST(req: NextRequest, { params }: { params: { id: string } }) {
-  const { id } = params;
+export async function POST(req: NextRequest, context: { params: { id: string } }) {
+  const { id } = context.params;
   // Get current views
   const { views } = await client.withConfig({ useCdn: false }).fetch(STARTUP_VIEWS_QUERY, { id });
   // Increment
@@ -13,8 +13,8 @@ export async function POST(req: NextRequest, { params }: { params: { id: string 
   return NextResponse.json({ views: views + 1 });
 }
 
-export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
-  const { id } = params;
+export async function GET(req: NextRequest, context: { params: { id: string } }) {
+  const { id } = context.params;
   const { views } = await client.withConfig({ useCdn: false }).fetch(STARTUP_VIEWS_QUERY, { id });
   return NextResponse.json({ views });
 }
